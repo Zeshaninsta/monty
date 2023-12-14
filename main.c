c3d024336f18bc5873a6502452304886c62c1466
zeshaninsta <zeshaninsta@gmail.com>
2023-12-14 14:25:03 -0500

    Worked Projects

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
+#define _GNU_SOURCE
 #include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point for the Monty interpreter
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
 int main(int argc, char *argv[])
 {
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    unsigned int line_number = 0;
+    stack_t *stack = NULL;
+
     /* Check for correct number of arguments */
     if (argc != 2)
     {
@@ -12,7 +25,7 @@ int main(int argc, char *argv[])
     }
 
     /* Open the Monty byte code file */
-    FILE *file = fopen(argv[1], "r");
+    file = fopen(argv[1], "r");
     if (file == NULL)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
@@ -20,7 +33,17 @@ int main(int argc, char *argv[])
     }
 
     /* Read the file line by line and execute the instructions */
+    while (getline(&line, &len, file) != -1)
+    {
+        line_number++;
+        /* Execute instruction here */
+        /* Call a function to process the instruction, passing stack, line, and line_number */
+    }
 
     fclose(file);
-    return 0;
+    if (line)
+        free(line);
+
+    (void)stack;
+    exit(EXIT_SUCCESS);
 }
